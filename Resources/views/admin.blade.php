82589e14748a8a56204c23640b6714e95d253677
B.Fatih KOZ <74361521+FatihKoz@users.noreply.github.com>
2021-03-27 13:22:58 +0300

    Update admin.blade.php

--- Resources/views/admin.blade.php
+++ Resources/views/admin.blade.php
@@ -12,11 +12,11 @@
         While using the widget you can define the options as below;
       </p>
       <p>
-        <b>@widget('Modules\JumpSeat\Widgets\JumpSeat')</b> this is module default, provides free travel<br>
-        <b>@widget('Modules\JumpSeat\Widgets\JumpSeat', ['price' => 'free'])</b> Same as above, free travel<br>
-        <b>@widget('Modules\JumpSeat\Widgets\JumpSeat', ['price' => 150])</b> for fixed ticket price (uses your settings -> currency)<br>
-        <b>@widget('Modules\JumpSeat\Widgets\JumpSeat', ['price' => 'auto'])</b> for automatic ticket price calculation with the default base price<br>
-        <b>@widget('Modules\JumpSeat\Widgets\JumpSeat', ['price' => 'auto', 'base' => 0.20])</b> for automatic price calculation with your base price per nautical miles<br><br>
+        <b>@ widget('Modules\JumpSeat\Widgets\JumpSeat')</b> this is module default, provides free travel<br>
+        <b>@ widget('Modules\JumpSeat\Widgets\JumpSeat', ['price' => 'free'])</b> Same as above, free travel<br>
+        <b>@ widget('Modules\JumpSeat\Widgets\JumpSeat', ['price' => 150])</b> for fixed ticket price (uses your settings -> currency)<br>
+        <b>@ widget('Modules\JumpSeat\Widgets\JumpSeat', ['price' => 'auto'])</b> for automatic ticket price calculation with the default base price<br>
+        <b>@ widget('Modules\JumpSeat\Widgets\JumpSeat', ['price' => 'auto', 'base' => 0.20])</b> for automatic price calculation with your base price per nautical miles<br><br>
         The base price is directly multiplied with the great circle distance between airports, so setting them anything above 0.50 may hurt your pilots' budgets.
       </p>
       <p>Thanks Nabeel and Andreas for their support during the development of this module.</p>
